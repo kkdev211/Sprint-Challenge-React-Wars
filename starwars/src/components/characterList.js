557bc8d40656396ab37a7cae33ef602c6c937100
kkdev211 <kkdev211@users.noreply.github.com>
2020-02-09 23:49:48 -0500

    checked work

--- starwars/src/components/characterList.js
+++ starwars/src/components/characterList.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
-// 2. Axios import
+
 import axios from "axios";
 import CharacterCard from "./characterCard";
 import { Container, Row } from "reactstrap";
 
-// 3. Set up components with empty div return
+
 const CharacterList = () => {
-  // 5. set up state
   const initialState = [];
   const [characters, setCharacters] = useState(initialState);
-  //4. set up useEffect
+  
   const effectCallbackFn = () => {
     console.log("calling effect fn!");
     axios
@@ -20,9 +19,7 @@ const CharacterList = () => {
       })
       .catch(error => console.log(error));
   };
-  // [] => only fire after initial render
-  // [state, stateTwo] => if any state value that is included in the 
-  // dependency array is CHANGED then the useEffect function is called again
+  
   useEffect(effectCallbackFn, []);
 
   console.log("initial render of characters", characters);
